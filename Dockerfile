3688cd7cab8ce030b31a1aaa9b0a6ae201538ded
Andrey Belym <belym.a.2105@gmail.com>
2019-05-30 13:51:11 +0300

    Modify example

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,4 @@
-FROM node:7.10.0
-
-# Add Tini
-ENV TINI_VERSION v0.14.0
-ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
-RUN chmod +x /tini
-ENTRYPOINT ["/tini", "--"]
+FROM node:10
 
 WORKDIR /app
 COPY package.json /app
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const http = require('http');
 const webdriver = require('selenium-webdriver')
 const browserstack = require('browserstack-local')
 const local = new browserstack.Local()
@@ -19,23 +20,31 @@ var options = {
 
 console.log('Starting tunnel...')
 
-local.start(options, function (error) {
-  if (error) {
-    console.error('Received error while starting tunnel', error)
-    process.exit(1)
-  }
-  console.log('Is Running', local.isRunning())
-  console.log('Started')
 
-  var driver = new webdriver.Builder().usingServer('http://hub.browserstack.com/wd/hub').withCapabilities(capabilities).build()
-  driver.get('http://www.browserstack.com').then(function () {
-    driver.getTitle().then(function (title) {
-      console.log(title)
-    })
-    driver.quit().then(function () {
-      local.stop(function () {
-        console.log('Stopped')
-      })
-    })
+const server = http
+  .createServer((request, response) => {
+    response.statusCode = 200;
+    response.end(`<html><head><title>Hello World</title></head><body><h1>HELLO WORLD</h1></body></html>`);
   })
-})
+  .listen(() => {
+    local.start(options, function (error) {
+      if (error) {
+        console.error('Received error while starting tunnel', error)
+        process.exit(1)
+      }
+      console.log('Is Running', local.isRunning())
+      console.log('Started')
+    
+      var driver = new webdriver.Builder().usingServer('http://hub.browserstack.com/wd/hub').withCapabilities(capabilities).build()
+      driver.get(`http://127.0.0.1:${server.address().port}`).then(function () {
+        driver.getTitle().then(function (title) {
+          console.log(title)
+        })
+        driver.quit().then(function () {
+          local.stop(function () {
+            console.log('Stopped')
+          })
+        })
+      })
+    })    
+  });
