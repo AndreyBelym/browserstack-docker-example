eb8540d54bcfd31146065b0fe67ea66c7712c1c3
Andrey Belym <belym.a.2105@gmail.com>
2019-06-04 18:16:03 +0300

    Disabled browserstack local

--- test.js
+++ test.js
@@ -2,7 +2,6 @@
 
 const http = require('http');
 const webdriver = require('selenium-webdriver')
-const browserstack = require('browserstack-local')
 const local = new browserstack.Local()
 
 var capabilities = {
@@ -27,24 +26,17 @@ const server = http
     response.end(`<html><head><title>Hello World</title></head><body><h1>HELLO WORLD</h1></body></html>`);
   })
   .listen(() => {
-    local.start(options, function (error) {
-      if (error) {
-        console.error('Received error while starting tunnel', error)
-        process.exit(1)
-      }
-      console.log('Is Running', local.isRunning())
-      console.log('Started')
+    console.log('Started')
     
-      var driver = new webdriver.Builder().usingServer('http://hub.browserstack.com/wd/hub').withCapabilities(capabilities).build()
-      driver.get(`http://127.0.0.1:${server.address().port}`).then(function () {
-        driver.getTitle().then(function (title) {
-          console.log(title)
-        })
-        driver.quit().then(function () {
-          local.stop(function () {
-            console.log('Stopped')
-          })
+    var driver = new webdriver.Builder().usingServer('http://hub.browserstack.com/wd/hub').withCapabilities(capabilities).build()
+    driver.get(`http://127.0.0.1:${server.address().port}`).then(function () {
+      driver.getTitle().then(function (title) {
+        console.log(title)
+      })
+      driver.quit().then(function () {
+        local.stop(function () {
+          console.log('Stopped')
         })
       })
-    })    
+    })   
   });
